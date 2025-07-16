b884cbd959ba0c9c4923659f4691d06ed1a00618
Ivan <elreizerwtf@gmail.com>
2025-07-16 12:12:55 -0300

    index fix

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,15 @@
-export { default as PdfManager } from "./organism/filepdf";
+export { default as PdfManager } from "./organism/filepdf"; //no es necesario
 export { default as Button } from "./atoms/Button";
 export { default as fileInput } from "./molecules/fileInput"
 export { default as FileList } from "./molecules/FileList"
+
+
+//FORM
+export { default as Input } from "./atoms/input";
+export { default as Label } from "./atoms/label";
+export { default as RadioButton } from "./atoms/radioButton";
+export { default as TextArea } from "./atoms/textArea";
+
+export { default as FormField } from "./molecules/formField";
+export { default as RadioGroup } from "./molecules/radioGroup";
+export { default as ContactForm } from "./organism/contactForm";
--- src/components/index.tsx
+++
@@ -1,9 +0,0 @@
-export { default as Input } from "./atoms/input";
-export { default as Label } from "./atoms/label";
-export { default as RadioButton } from "./atoms/radioButton";
-export { default as TextArea } from "./atoms/textArea";
-
-export { default as FormField } from "./molecules/formField";
-export { default as RadioGroup } from "./molecules/radioGroup";
-
-export { default as ContactForm } from "./organism/contactForm";
