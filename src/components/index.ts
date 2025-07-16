cebe43df775ef6c16d0358b528a1a31b3ef8b69f
LuisCruzA <langel18.1818@gmail.com>
2025-07-16 10:04:22 -0600

    se eliminaron componentes para seleccion de archivospdf

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,3 @@
-export { default as PdfManager } from "./organism/filepdf"; //no es necesario
-export { default as Button } from "./atoms/Button";
-export { default as fileInput } from "./molecules/fileInput"
-export { default as FileList } from "./molecules/FileList"
 
 
 //FORM
