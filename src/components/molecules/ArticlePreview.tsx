bd9ecea2502a257e3b172a8da68e0a23f283de05
Ivan <elreizerwtf@gmail.com>
2025-07-16 13:31:11 -0300

    modificar ArticlePreview, intentar quitar el key

--- src/components/molecules/ArticlePreview.tsx
+++ src/components/molecules/ArticlePreview.tsx
@@ -8,15 +8,15 @@ type Props = {
     extracto: string;
     link: string;
     alt: string;
-    src?: string;
+    src: string;
     key: string;
 };
 
-export default function ArticlePreview({titulo, extracto, link, alt, src, key}: Props) {
+export default function ArticlePreview({titulo, extracto, link, alt, src, key}: Props) {          //intentar sacar el key
 
     return <article key={key} className='bg-white rounded-xl shadow-md overflow-hidden flex flex-col'>
                 <div className="h-40 bg-gray-200 flex items-center justify-center">
-                    <NewsImage src={key} alt={alt}/>
+                    <NewsImage src={src} alt={alt}/>
                 </div>
                 <div className="p-6 flex flex-col flex-1">
                     <Heading>{titulo}</Heading>
